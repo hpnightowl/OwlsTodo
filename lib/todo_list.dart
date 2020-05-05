8a7c1647b8fd4593baa0fbfbf7503e0cc9c3602e
hpnightowl <iamhp2k@gmail.com>
2020-05-06 02:39:24 +0530

    Tasks: Added init Data types Needed

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
++ import 'package:owlstask/todo.dart';
 
 class TodoList extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
+     List<Todo> todos = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
